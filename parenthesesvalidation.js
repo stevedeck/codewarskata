f43f8c0ab39eef5c56f2db4b8990df46964d1850
Steve Deck <71664388+stevedeck@users.noreply.github.com>
2021-05-03 14:01:42 -0600

    update include check for parentheses configuration
    update to include check of parentheses orientation

--- parenthesesvalidation.js
+++ parenthesesvalidation.js
@@ -1,15 +1,24 @@
 function validParentheses(parens){
 
   let parenSum = 0;
+  let brackets = [];
   
   for (let i=0; i < parens.length; i++) {
     
     switch (parens[i]){
       case "(":
         parenSum++;
+        brackets.push(parens[i]);
         break;
       case ")":
         parenSum--;
+        if (brackets.Count < 0) return false;
+        
+        let open = brackets.pop();
+        
+        if (parens[i] == ')' && open != '(' ) {
+          return false;
+        }
         break;
     }
   } 
